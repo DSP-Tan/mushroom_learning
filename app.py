d5e815c01511ba8c9d0aa65e73f1d4ea4af0fd69
Stella Givanoudi <stellayiva@gmail.com>
2022-03-03 10:01:29 +0100

    file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import requests
+from api.fast import create_file
 
 #url = 'https://taxifare.lewagon.ai/predict' --> to be updated with Stella
 
@@ -23,8 +24,8 @@ image = st.file_uploader(label, type=None, accept_multiple_files=False, key=None
 cols = st.columns(3)
 if image:
     if cols[1].button('Identify your Mushroom'):
-        #response = requests.get(url,image)
-        cols[1].write('Red Mushroom')
+        response = create_file(image)
+        cols[1].write(response)
 
 
 
