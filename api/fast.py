91312472ede395f53ad3e0fbdfe4c62797bd2ac5
Stella Givanoudi <stellayiva@gmail.com>
2022-03-04 11:33:32 +0100

    file

--- api/fast.py
+++ api/fast.py
@@ -1,3 +1,4 @@
+from black import out
 from fastapi import FastAPI,File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from mushroom_learning.gcp import load_from_gcp
@@ -28,20 +29,20 @@ def index():
 def create_file(file: bytes = File(...)):
     #The file is in bytes format. Convert byte to bits.
     from_byte_to_bits = bytearray(file)
-    # # #Convert bits to array.
+    # # # # #Convert bits to array.
     from_bits_to_array = np.asarray(from_byte_to_bits, dtype="uint8")
-    # # #Decode the array to image. This will be used from the model.
+    # # # #Decode the array to image. This will be used from the model.
     decode_img = cv.imdecode(from_bits_to_array,cv.IMREAD_COLOR)
-    # # #Save the image. Just to check it
-    # cv2.imwrite('output.png',decode_img)
+    # # # #Save the image. Just to check it
+    # cv.imwrite('output.png',decode_img)
 
     model = load_from_gcp()
 
-    # # # # make prediction
+    # # # # # make prediction
     img_height = 224
     img_width = 224
     decode_img_reshaped = tf.keras.utils.load_img('output.png', target_size=(img_height, img_width))
-    # decode_img_reshaped = cv.resize(decode_img, (img_height, img_width))
+    # # # decode_img_reshaped = cv.resize(decode_img, (img_height, img_width))
     img_array = tf.keras.utils.img_to_array(decode_img_reshaped)
     img_array_expand = tf.expand_dims(img_array, 0) # Create a batch
     results = model.predict(img_array_expand)
--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import requests
 from api.fast import create_file
+import cv2 as cv
+from PIL import Image
+from tensorflow.keras import utils
+import os
 
 st.set_page_config(
         page_title="Mushroom Magic", # => Quick reference - Streamlit
@@ -22,11 +26,19 @@ image = st.file_uploader(label, type=None, accept_multiple_files=False, key=None
 
 cols = st.columns(3)
 URL = 'http://127.0.0.1:8000/predict/'
+
+##code 1
 if image:
     if cols[1].button('Identify your Mushroom'):
+        file_details = {"FileName":image.name,"FileType":image.type}
+        st.write(file_details)
+        with open(os.path.join("/home/stella/code/DSP-Tan/mushroom_learning",'output.png'),"wb") as f:
+            f.write(image.getbuffer())
+        st.success("ouput")
         files = {'file':image}
         response = requests.get(URL, data=files)
         cols[1].write(response.json())
+
 
 
 if image:
