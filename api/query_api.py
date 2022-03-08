637488859708a595908ce86b219c3b2c738faa76
Daniel Tanner <danielsptanner @gmail.com>
2022-03-08 12:18:02 +0000

    Who even knows what's going on anymore. Perhaps It would be wise to go back to the versions of these files I made before docker broke my laptop with it's decrepid images.

--- api/query_api.py
+++ api/query_api.py
@@ -1,4 +1,6 @@
 import requests
+import os
+
 URL_base    ='https://mushroom-docker-lpuaioudtq-ew.a.run.app'
 URL_base    ='http://0.0.0.0:8000/'
 
@@ -7,18 +9,23 @@ URL_size    = URL_base+'size/'
 URL_predict = URL_base+'predict/'
 
 
-mush_path='./FS2013PIC82005485.JPG'
+mush_path='FS2013PIC82005485.JPG'
+mush_path='APE2017-9186528_HyZ-iZy6vg.JPG'
 
 print('Response from base URL:')
 response = requests.get(URL_base)
 print(response.json())
 
+image_path=os.path.join(os.getcwd(),mush_path)
+#with open(image_path,"wb") as f:
+#    f.write(image.getbuffer())
 
 files = {'mush': mush_path  }
 
 print('Response from size URL:')
-response = requests.get(URL_base,data=files)
+response = requests.get(URL_size,data=files)
 print(response.json())
 
-#print('Response from predict URL:')
+print('Response from predict URL:')
 #print(response.json())
+
