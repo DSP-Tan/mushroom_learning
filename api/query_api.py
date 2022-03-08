3013a1a43dec1748786138ecc0bd79dd148056ab
Daniel Tanner <danielsptanner @gmail.com>
2022-03-08 10:20:01 +0000

    testing api

--- api/query_api.py
+++ api/query_api.py
@@ -1,19 +1,24 @@
 import requests
+URL_base    ='https://mushroom-docker-lpuaioudtq-ew.a.run.app'
+URL_base    ='http://0.0.0.0:8000/'
 
-URL_base    = 'https://mushroom-docker-lpuaioudtq-ew.a.run.app/'
+
 URL_size    = URL_base+'size/'
 URL_predict = URL_base+'predict/'
+
+
+mush_path='./FS2013PIC82005485.JPG'
 
 print('Response from base URL:')
 response = requests.get(URL_base)
 print(response.json())
 
 
+files = {'mush': mush_path  }
 
-
-#print('Response from size URL:')
-#response = requests.get(URL_base)
-#print(response.json())
+print('Response from size URL:')
+response = requests.get(URL_base,data=files)
+print(response.json())
 
 #print('Response from predict URL:')
 #print(response.json())
